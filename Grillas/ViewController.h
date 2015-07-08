dc6616af1dd069ee1559c68b665fdc595295807a
cristian <cp@mambo.pe>
2015-07-08 10:27:11 -0500

    Initial Commit

---
+++ Grillas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Grillas
+//
+//  Created by Cristian on 8/07/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Grillas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Grillas
+//
+//  Created by Cristian on 8/07/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
