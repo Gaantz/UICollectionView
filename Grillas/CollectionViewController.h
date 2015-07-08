eec19c94f97901eb30d71acb438e2e3276460be2
cristian <cp@mambo.pe>
2015-07-08 16:02:55 -0500

    collection news v1

---
+++ Grillas/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  Grillas
+//
+//  Created by Cristian on 8/07/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
